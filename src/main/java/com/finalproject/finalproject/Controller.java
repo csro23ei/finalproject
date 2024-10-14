e6ebc409c929d8462d276adde6daa0b2562bc484
csro23ei <dante030724@gmail.com>
2024-10-14 11:26:24 +0200

    deploy

---
+++ src/main/java/com/finalproject/finalproject/Controller.java
@@ -0,0 +1,14 @@
+package com.finalproject.finalproject;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class Controller {
+    @GetMapping
+    public String getIndex() {
+        return "{'messages': 'Hello world'}";
+    }
+
+}
