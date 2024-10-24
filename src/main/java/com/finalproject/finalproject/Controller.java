782581e72203257d47ec68ba3d8860f413a40b8c
csro23ei <dante030724@gmail.com>
2024-10-24 09:53:12 +0200

    ger up stripe går på realtids kom

--- src/main/java/com/finalproject/finalproject/Controller.java
+++ src/main/java/com/finalproject/finalproject/Controller.java
@@ -1,14 +1,19 @@
 package com.finalproject.finalproject;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class Controller {
-    @GetMapping
-    public String getIndex() {
-        return "{'messages': 'Hello world 8'}";
-    }
 
+    @Value("${nonsecret.key}")
+    String nonsecretkey;
+
+    // Combine both messages into one method
+    @GetMapping("/")
+    public String getIndexAndKey() {
+        return "{'messages': 'Hello world 11', 'nonsecretkey': '" + nonsecretkey + "'}";
+    }
 }
